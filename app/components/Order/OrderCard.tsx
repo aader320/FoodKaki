e3fe20534744a7f7e6d037e0f9bd2b8a19f32700
ChromeEagle99 <chromeeagle99@gmail.com>
2024-05-25 19:47:56 +0800

    grab/ntuc to more details

--- app/components/Order/OrderCard.tsx
+++ app/components/Order/OrderCard.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 interface OrderCardProps {
   image: string;
   title: string;
-  price: string;
+  href: string;
   selected: boolean;
   onSelect: () => void;
 }
 
-const OrderCard: React.FC<OrderCardProps> = ({ image, title, selected, onSelect }) => {
-  return (
+const OrderCard: React.FC<OrderCardProps> = ({ image, title, selected, onSelect, href }) => {
+    const router = useRouter();
+
+    const handleCardClick = () => {
+        router.push(href);
+      };
+
+    return (
     <div
       className={`card shadow-lg cursor-pointer ${selected ? 'border-2 border-primary' : ''}`}
-      onClick={onSelect}
+      onClick={handleCardClick}
     >
       <figure>
         <img src={image} alt={title} className="w-full h-48 object-cover" />
