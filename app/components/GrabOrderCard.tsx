92e073905122719dc0c962551b66be0a7824bb90
ChromeEagle99 <chromeeagle99@gmail.com>
2024-05-25 18:23:58 +0800

    grabOrderList

--- app/components/GrabOrderCard.tsx
+++ app/components/GrabOrderCard.tsx
@@ -1,30 +1,28 @@
-
 import React from 'react';
 
-interface IngredientCardProps {
+interface GrabOrderCardProps {
   image: string;
-  name: string;
+  title: string;
   price: string;
   selected: boolean;
   onSelect: () => void;
 }
 
-const IngredientCard: React.FC<IngredientCardProps> = ({ image, name, price, selected, onSelect }) => {
+const GrabOrderCard: React.FC<GrabOrderCardProps> = ({ image, title, price, selected, onSelect }) => {
   return (
     <div
       className={`card shadow-lg cursor-pointer ${selected ? 'border-2 border-primary' : ''}`}
       onClick={onSelect}
     >
       <figure>
-        <img src={image} alt={name} className="w-full h-48 object-cover" />
+        <img src={image} alt={title} className="w-full h-24 object-cover" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 className="card-title">{title}</h2>
         <p className="text-gray-500">{price}</p>
       </div>
     </div>
   );
 };
 
-export default IngredientCard;
-
+export default GrabOrderCard;
--- app/grabOrderList/page.tsx
+++ app/grabOrderList/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import IngredientList from '../components/IngredientList';
+import GrabOrderList from '../components/GrabOrderList';
 
 const Home: React.FC = () => {
   return (
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       <main className="flex-grow overflow-auto mt-16 mb-16">
         <div className="container mx-auto p-4">
           <div className="mt-4">
-            <IngredientList />
+            <GrabOrderList/>
           </div>
         </div>
       </main>
