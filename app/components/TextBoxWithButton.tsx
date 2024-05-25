8a1e98860c0129f0aad0de774a994d277534fdde
Richmond Choo Tze Yong <90663945+Taterr@users.noreply.github.com>
2024-05-25 19:03:37 +0800

    Update TextBoxWithButton.tsx

--- app/components/TextBoxWithButton.tsx
+++ app/components/TextBoxWithButton.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Use next/navigation
 
 const TextBoxWithButton: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  const router = useRouter();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (inputValue.trim() !== '') {
+      // Assuming you have set up Zustand as described earlier
+      // setSearchQuery(inputValue);  // Uncomment if you are using Zustand
+      router.push('/selectOrder'); // Replace with your desired route
+    }
+  };
+
   return (
     <div className="flex items-center border rounded shadow p-2">
       <input
         type="text"
         placeholder="What do you want to eat today?"
+        value={inputValue}
+        onChange={handleInputChange}
         className="input input-bordered w-full"
       />
-      <button className="btn btn-primary ml-2">Enter</button>
+      <button onClick={handleButtonClick} className="btn btn-primary ml-2">Enter</button>
     </div>
   );
 };
